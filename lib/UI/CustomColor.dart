b254b7fecc0723060a7756e8c369348347f6e48e
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-03 16:33:23 +0200

    create custom color class

---
+++ lib/UI/CustomColor.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class CustomColor {
+
+  static const Color _darkPurpleBlueColor = Color(0xFF0A0E21);
+
+  get darkPurpleBlueColor => _darkPurpleBlueColor;
+}
