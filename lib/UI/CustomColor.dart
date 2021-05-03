ecee6e80c46dce4a7fb5ab2e9eb031323d637c9d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-03 18:39:53 +0200

    delete CustomColor, create an AppConst class and moove values from deleted class to the new AppConst class

--- lib/UI/CustomColor.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class CustomColor {
-  static const Color darkPurpleBlueColor = Color(0xFF0A0E21);
-  static const Color lighterDarkPurpleBlueColor = Color(0xFF1D1E33);
-}
-
---
+++ lib/Utils/AppConst.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppConst {
+  static const mainBottomCardHeight = 80;
+
+  static const Color colorIntenseDark = Color(0xFF0A0E21);
+  static const Color colorLighterDark = Color(0xFF1D1E33);
+  static const Color colorFlashyPink = Color(0xFFEB1555);
+  }
+  
