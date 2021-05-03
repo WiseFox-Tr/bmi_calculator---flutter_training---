b66fb38e822802d9adde9a529f032652fb3bbc5c
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-03 19:16:39 +0200

    add some new const

--- lib/Utils/AppConst.dart
+++ lib/Utils/AppConst.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 
 class AppConst {
+  //size
   static const double heightBottomCard = 80.0;
+  static const double sizeIcon = 80.0;
+  static const double fontSizeRegular = 18.0;
 
+  //color
   static const Color colorIntenseDark = Color(0xFF0A0E21);
   static const Color colorLighterDark = Color(0xFF1D1E33);
   static const Color colorFlashyPink = Color(0xFFEB1555);
+  static const Color colorLightGrey = Color(0xFF8D8E98);
   }
 
