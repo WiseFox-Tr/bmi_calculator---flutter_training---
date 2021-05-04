1c389ba1b5665f52f82aabff20dfbc4e9a309e32
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-04 14:37:09 +0200

    rename color const for active card & add an inactive card color

--- lib/Utils/AppConst.dart
+++ lib/Utils/AppConst.dart
@@ -3,12 +3,13 @@ import 'package:flutter/material.dart';
 class AppConst {
   //size
   static const double heightBottomCard = 80.0;
-  static const double sizeIcon = 80.0;
+  static const double sizeIcon = 70.0;
   static const double fontSizeRegular = 18.0;
 
   //color
   static const Color colorIntenseDark = Color(0xFF0A0E21);
-  static const Color colorLighterDark = Color(0xFF1D1E33);
+  static const Color colorInactiveCard = Color(0xFF111328);
+  static const Color colorActiveCard = Color(0xFF1D1E33);
   static const Color colorFlashyPink = Color(0xFFEB1555);
   static const Color colorLightGrey = Color(0xFF8D8E98);
   }
