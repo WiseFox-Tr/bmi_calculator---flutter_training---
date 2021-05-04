34c613dd5824598579b5c6f1a2fd530e713f1a63
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-04 20:14:04 +0200

    add a Gesture detector and a function attribute onPress for custom ReusableCard widget

--- lib/UI/widget/ReusableCard.dart
+++ lib/UI/widget/ReusableCard.dart
@@ -5,17 +5,21 @@ class ReusableCard extends StatelessWidget {
 
   final Color color;
   final Widget child;
+  final Function onPress;
 
-  ReusableCard({this.color = AppConst.colorActiveCard, this.child});
+  ReusableCard({this.color = AppConst.colorCardActive, this.child, this.onPress});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: child,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: child,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
