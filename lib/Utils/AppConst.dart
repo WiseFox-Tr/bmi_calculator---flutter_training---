7af509b3d180754c718f33409143c6c6f02bf72c
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-04 16:11:50 +0200

    rename active & inactive colors const & modify color fo inactiveCardColor

--- lib/Utils/AppConst.dart
+++ lib/Utils/AppConst.dart
@@ -8,8 +8,8 @@ class AppConst {
 
   //color
   static const Color colorIntenseDark = Color(0xFF0A0E21);
-  static const Color colorInactiveCard = Color(0xFF111328);
-  static const Color colorActiveCard = Color(0xFF1D1E33);
+  static const Color colorCardInactive = Color(0xFF090B17);
+  static const Color colorCardActive = Color(0xFF1D1E33);
   static const Color colorFlashyPink = Color(0xFFEB1555);
   static const Color colorLightGrey = Color(0xFF8D8E98);
   }
