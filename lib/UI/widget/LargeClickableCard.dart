6c542b24a9fda7819806b90092ba25d29548f17d
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-06 20:29:34 +0200

    create a LargeClickableCard Widget for calculate BMI

---
+++ lib/UI/widget/LargeClickableCard.dart
@@ -0,0 +1,40 @@
+import 'package:bmi_calculator/Utils/AppConst.dart';
+import 'package:flutter/material.dart';
+
+class LargeClickableCard extends StatelessWidget {
+  ///provides a card with a width as large as possible
+  ///this card is clickable with an onTap callback
+
+  final Function onTap;
+  final String cardTitle;
+  final double height;
+  final Color backgroundColor;
+  final TextStyle textStyle;
+
+  LargeClickableCard({
+    @required this.onTap,
+    @required this.cardTitle,
+    this.height = 50.0,
+    this.backgroundColor = AppConst.colorFlashyPink,
+    this.textStyle = AppConst.regularTextStyle,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        alignment: Alignment.center,
+        padding: EdgeInsets.only(bottom: 5.0),
+        color: backgroundColor,
+        width: double.infinity,
+        height: height,
+        margin: EdgeInsets.only(top: 10.0),
+        child: Text(
+          cardTitle.toUpperCase(),
+          style: textStyle,
+        ),
+      ),
+    );
+  }
+}
