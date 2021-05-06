3370735bf9e05d5f918f2474aae9c7a85ce7a3d3
Maxime Traverse <maxime.Traverse@orange.fr>
2021-05-06 20:30:44 +0200

    Make UI

--- lib/UI/screens/resultScreen.dart
+++ lib/UI/screens/resultScreen.dart
@@ -1,3 +1,5 @@
+import 'package:bmi_calculator/UI/widget/LargeClickableCard.dart';
+import 'package:bmi_calculator/UI/widget/ReusableCard.dart';
 import 'package:bmi_calculator/Utils/AppConst.dart';
 import 'package:flutter/material.dart';
 
@@ -8,9 +10,52 @@ class ResultScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(AppConst.strAppTitle),
       ),
-      body: Container(
-        alignment: Alignment.center,
-        child: Text('Result'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(16.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                'Your result',
+                style: AppConst.numberTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    'overweight'.toUpperCase(),
+                    style: AppConst.regularTextStyle.copyWith(
+                      color: AppConst.colorFlashyGreen,
+                    ),
+                  ),
+                  Text(
+                    '80',
+                    style: AppConst.resultNumberTextStyle,
+                  ),
+                  Text(
+                    'lorem ipsum sentence',
+                    style: AppConst.regularTextStyle.copyWith(
+                      fontWeight: FontWeight.w400
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          LargeClickableCard(
+            onTap: () => Navigator.pop,
+            cardTitle: 're-calculate',
+            height: AppConst.heightBottomCard,
+          ),
+        ],
       ),
     );
   }
